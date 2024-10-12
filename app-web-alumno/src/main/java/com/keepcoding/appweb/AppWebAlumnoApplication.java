e68f885545a8e006ec75600ec90012fe12c1dbec
Natalia Tsyleva <tsylevanatalia@gmail.com>
2024-10-12 10:45:44 +0200

    the project structure has been prepared, including the main packages, created entity Alumno

---
+++ app-web-alumno/src/main/java/com/keepcoding/appweb/AppWebAlumnoApplication.java
@@ -0,0 +1,13 @@
+package com.keepcoding.appweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebAlumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebAlumnoApplication.class, args);
+	}
+
+}
---
+++ app-web-alumno/src/main/java/com/keepcoding/appweb/entity/Alumno.java
@@ -0,0 +1,108 @@
+package com.keepcoding.appweb.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno implements Serializable{
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false, length = 50)
+	private String nombre;
+	@Column(nullable = false, length = 50)
+	private String apellido;
+	private int telefono;
+	@Column(nullable = false, length = 50, unique = true)
+	private String email;
+	@Column(nullable = false, length = 50, unique = true)
+	private String dni;
+	private String fecha_nac;
+	
+	
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+	public String getFecha_nac() {
+		return fecha_nac;
+	}
+
+
+	public void setFecha_nac(String fecha_nac) {
+		this.fecha_nac = fecha_nac;
+	}
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+}
